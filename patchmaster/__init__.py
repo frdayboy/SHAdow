e16ac832af7af248489a4aee52a92504e7f0bc37
CodeJou <joumaa_Family@icloud.com>
2020-06-23 20:50:11 -0400

    Fixed supported A4 devs

--- patchmaster/__init__.py
+++ patchmaster/__init__.py
@@ -1,3 +1,4 @@
+#THIS FILE IS PART OF Project SHAdow
 #patchmaster v0.0.6
 
 exported_patches = []
@@ -17,7 +18,18 @@ class Patchset:
 def initialize_all_patches(sn):
     exported_patches.append(Patchset("patchmaster/bin/SHAtter-shellcode.bin", "[Stage1] "))
     if "CPID:8930" in sn:
-        exported_patches.append(Patchset("patchmaster/bin/LLB.n81ap.PATCHED.RELEASE.img3", "[Stage2] "))
+    	if "BDID:08" in sn:
+		exported_patches.append(Patchset("patchmaster/bin/LLB.n81ap.PATCHED.RELEASE.img3", "[Stage2] "))
+	elif "BDID:02" in sn:
+		pass
+	elif "BDID:0" in sn:
+		pass
+	elif "BDID:04" in sn:
+		pass
+	elif "BDID:06" in sn:
+		pass
+	elif "BDID:10" in sn:
+		pass    
     else:
         print("[SHAdow] Device is not compatible.")
         sys.exit(1)
